b6ff1f41217bfb7ceb78aeb1933176f3412bcd2d
Shahin M Hashim <shahinmhashim@gmail.com>
2024-10-23 22:24:14 +0530

    fixed the todo expanding and collapsing feature

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,9 @@ import ThemeContext from "./providers/ThemeContext";
 function App() {
   const { theme, setTheme } = useContext(ThemeContext);
 
-  const [isExpanded, setIsExpanded] = useState(false);
+  const [expandedTodo, setExpandedTodo] = useState(null);
 
-  const toggleExpand = () => {
-    setIsExpanded(!isExpanded);
-  };
+  const toggleExpand = (id) => setExpandedTodo(expandedTodo === id ? null : id);
 
   return (
     <main className={`app theme-${theme} bg-color`}>
@@ -40,115 +38,223 @@ function App() {
           </div>
           <div className="grid grid-cols-1 md:grid-cols-[3fr,1fr]">
             <div className="flex flex-col md:border-r-2 border-r-gray-300 h-[74vh] overflow-auto">
-              <div className="flex items-center justify-between pl-5 border-b-2">
-                <div className="py-3 text-sm md:text-base">
-                  Learn Front End Development
+              <div id="123" className="">
+                <div className="flex items-center justify-between h-12 pl-5 border-b-2">
+                  <div className="py-3 text-sm md:text-base">
+                    Learn Front End Development
+                  </div>
+                  <div className="flex flex-shrink-0 h-full">
+                    <div className="flex items-center px-3 bg-blue-400">
+                      <img
+                        alt="expand"
+                        src="assets/icons/expand-up.png"
+                        className="h-6 cursor-pointer"
+                        style={{
+                          transform:
+                            expandedTodo === "123"
+                              ? "rotate(180deg)"
+                              : "rotate(0)",
+                          transition: "transform 0.3s linear 0.1s",
+                        }}
+                        onClick={() => toggleExpand("123")}
+                      />
+                    </div>
+                    <div className="flex items-center px-3 bg-green-400">
+                      {expandedTodo === "123" ? (
+                        <img
+                          alt="edit"
+                          src="assets/icons/edit.svg"
+                          className="h-6 cursor-pointer"
+                        />
+                      ) : (
+                        <img
+                          alt="edit"
+                          src="assets/icons/done.png"
+                          className="h-6 cursor-pointer"
+                        />
+                      )}
+                    </div>
+                    <div className="relative flex items-center px-3 bg-red-400 group">
+                      <img
+                        alt="delete"
+                        src="assets/icons/delete.svg"
+                        className="h-6 cursor-pointer"
+                      />
+                    </div>
+                  </div>
                 </div>
-                <div className="flex flex-shrink-0 h-full">
-                  <div className="flex items-center px-3 bg-blue-400">
-                    <img
-                      alt="expand"
-                      src="assets/icons/expand-up.png"
-                      className="h-6 cursor-pointer"
-                      onClick={toggleExpand}
-                    />
-                  </div>
-                  <div className="flex items-center px-3 bg-green-400">
-                    <img
-                      alt="done"
-                      src="assets/icons/done.png"
-                      className="h-6 cursor-pointer"
-                    />
-                  </div>
-                  <div className="relative flex items-center px-3 bg-red-400 group">
-                    <img
-                      alt="delete"
-                      src="assets/icons/delete.svg"
-                      className="h-6 cursor-pointer"
-                    />
-                  </div>
-                </div>
-              </div>
 
-              {/* Edit Todo */}
-              <div
-                className="overflow-hidden transition-all duration-500 ease-in-out border-b-2 bg-slate-300"
-                style={{
-                  maxHeight: isExpanded ? `500px` : "0",
-                  padding: isExpanded ? "1.25rem" : "0 1.25rem 0 1.25rem",
-                }}
-              >
-                <form className="flex flex-col">
-                  <input
-                    type="text"
-                    className="p-2 mb-3 border-2 rounded-md"
-                    placeholder="Enter New Name"
-                  />
-                  <textarea
-                    rows={3}
-                    className="p-2 mb-3 border-2 rounded-md"
-                    placeholder="Enter New Description"
-                  ></textarea>
-                  <input type="date" className="p-2 mb-3 border-2 rounded-md" />
-                  <div className="flex gap-3">
-                    <button className="text-white bg-btn hover:bg-btn-hover">
-                      Update
-                    </button>
-                    <button className="text-white bg-btn hover:bg-btn-hover">
-                      Cancel
-                    </button>
+                {/* Expanded Todo Details */}
+                <div
+                  className="border-b-2 bg-slate-300"
+                  style={{
+                    overflow: "hidden",
+                    maxHeight: expandedTodo === "123" ? "2000px" : "0",
+                    padding:
+                      expandedTodo === "123"
+                        ? "1.25rem"
+                        : "0 1.25rem 0 1.25rem",
+                    transition: "all 0.5s ease-in-out",
+                  }}
+                >
+                  <div className="flex flex-col justify-between gap-3 mb-3 md:flex-row md:m-0">
+                    <h4 className="text-lg font-semibold ">
+                      Learn Front End Development
+                    </h4>
+                    <div>
+                      <p>
+                        <span className="font-semibold text-blue-500">
+                          Created:&nbsp;
+                        </span>
+                        2022-06-31
+                      </p>
+                      <p>
+                        <span className="font-semibold text-red-500">
+                          Due:&nbsp;
+                        </span>
+                        2022-07-05
+                      </p>
+                    </div>
                   </div>
-                </form>
-              </div>
-
-              <div className="flex items-center justify-between pl-5 border-b-2">
-                <div className="py-3 text-sm md:text-base">
-                  Learn Front End Development
-                </div>
-                <div className="flex flex-shrink-0 h-full">
-                  <div className="flex items-center px-3 bg-blue-400">
-                    <img
-                      alt="expand"
-                      src="assets/icons/expand-up.png"
-                      className="h-6 cursor-pointer"
-                      onClick={toggleExpand}
-                    />
-                  </div>
-                  <div className="flex items-center px-3 bg-green-400">
-                    <img
-                      alt="edit"
-                      src="assets/icons/edit.svg"
-                      className="h-6 cursor-pointer"
-                    />
-                  </div>
-                  <div className="relative flex items-center px-3 bg-red-400 group">
-                    <img
-                      alt="delete"
-                      src="assets/icons/delete.svg"
-                      className="h-6 cursor-pointer"
-                    />
-                  </div>
-                </div>
-              </div>
-
-              {/* Todo Expanded */}
-              <div
-                className="overflow-hidden transition-all duration-500 ease-in-out border-b-2 bg-slate-300"
-                style={{
-                  maxHeight: isExpanded ? `500px` : "0",
-                  padding: isExpanded ? "1.25rem" : "0 1.25rem 0 1.25rem",
-                }}
-              >
-                <h4 className="mb-3">Name: Learn Front End Development</h4>
-                <div>
-                  <p className="mb-3">
-                    Description: <br /> Learn HTML, CSS, JavaScript, React, and
-                    Tailwind
+                  <h5 className="mb-1 text-lg font-bold underline underline-offset-4">
+                    Description
+                  </h5>
+                  <p>
+                    Ab omnis mollitia voluptas. Quae voluptas ullam at et optio
+                    id alias dolorem. Omnis totam sit adipisci vero. Deleniti
+                    praesentium commodi incidunt. Reprehenderit animi doloremque
+                    quia voluptatem ratione. Eaque ut et aut cupiditate et. Aut
+                    vero possimus animi porro. Fugit dolorum quis quidem debitis
+                    natus explicabo quibusdam aspernatur dolores. Vero
+                    consectetur maiores eaque incidunt consequatur tenetur neque
+                    dolore. A harum accusantium. Perferendis aut odio sunt
+                    asperiores repudiandae et. Eius et similique quidem voluptas
+                    nihil possimus natus commodi. Optio est quas. Tempora at
+                    ipsa ipsum deserunt dolorem explicabo. Ab omnis mollitia
+                    voluptas. Quae voluptas ullam at et optio id alias dolorem.
+                    Omnis totam sit adipisci vero. Deleniti praesentium commodi
+                    incidunt. Reprehenderit animi doloremque quia voluptatem
+                    ratione. Eaque ut et aut cupiditate et. Aut vero possimus
+                    animi porro. Fugit dolorum quis quidem debitis natus
+                    explicabo quibusdam aspernatur dolores. Vero consectetur
+                    maiores eaque incidunt consequatur tenetur neque dolore. A
+                    harum accusantium. Perferendis aut odio sunt asperiores
+                    repudiandae et. Eius et similique quidem voluptas nihil
+                    possimus natus commodi. Optio est quas. Tempora at ipsa
+                    ipsum deserunt dolorem explicabo.
                   </p>
-                  <p>Date: 2022-12-31</p>
+                </div>
+              </div>
+
+              <div id="256" className="">
+                <div className="flex items-center justify-between h-12 pl-5 border-b-2">
+                  <div className="py-3 text-sm md:text-base">
+                    Learn Front End Development
+                  </div>
+                  <div className="flex flex-shrink-0 h-full">
+                    <div className="flex items-center px-3 bg-blue-400">
+                      <img
+                        alt="expand"
+                        src="assets/icons/expand-up.png"
+                        className="h-6 cursor-pointer"
+                        style={{
+                          transform:
+                            expandedTodo === "256"
+                              ? "rotate(180deg)"
+                              : "rotate(0)",
+                          transition: "transform 0.3s linear 0.1s",
+                        }}
+                        onClick={() => toggleExpand("256")}
+                      />
+                    </div>
+                    <div className="flex items-center px-3 bg-green-400">
+                      {expandedTodo === "256" ? (
+                        <img
+                          alt="edit"
+                          src="assets/icons/edit.svg"
+                          className="h-6 cursor-pointer"
+                        />
+                      ) : (
+                        <img
+                          alt="edit"
+                          src="assets/icons/done.png"
+                          className="h-6 cursor-pointer"
+                        />
+                      )}
+                    </div>
+                    <div className="relative flex items-center px-3 bg-red-400 group">
+                      <img
+                        alt="delete"
+                        src="assets/icons/delete.svg"
+                        className="h-6 cursor-pointer"
+                      />
+                    </div>
+                  </div>
+                </div>
+
+                {/* Expanded Todo Details */}
+                <div
+                  className="border-b-2 bg-slate-300"
+                  style={{
+                    overflow: "hidden",
+                    maxHeight: expandedTodo === "256" ? "1000px" : "0",
+                    padding:
+                      expandedTodo === "256"
+                        ? "1.25rem"
+                        : "0 1.25rem 0 1.25rem",
+                    transition: "all 0.5s ease-in-out",
+                  }}
+                >
+                  <div className="flex flex-col justify-between gap-3 mb-3 md:flex-row md:m-0">
+                    <h4 className="text-lg font-semibold ">
+                      Learn Front End Development
+                    </h4>
+                    <div>
+                      <p>
+                        <span className="font-semibold text-blue-500">
+                          Created:&nbsp;
+                        </span>
+                        2022-06-31
+                      </p>
+                      <p>
+                        <span className="font-semibold text-red-500">
+                          Due:&nbsp;
+                        </span>
+                        2022-07-05
+                      </p>
+                    </div>
+                  </div>
+                  <h5 className="mb-1 text-lg font-bold underline underline-offset-4">
+                    Description
+                  </h5>
+                  <p>
+                    Ab omnis mollitia voluptas. Quae voluptas ullam at et optio
+                    id alias dolorem. Omnis totam sit adipisci vero. Deleniti
+                    praesentium commodi incidunt. Reprehenderit animi doloremque
+                    quia voluptatem ratione. Eaque ut et aut cupiditate et. Aut
+                    vero possimus animi porro. Fugit dolorum quis quidem debitis
+                    natus explicabo quibusdam aspernatur dolores. Vero
+                    consectetur maiores eaque incidunt consequatur tenetur neque
+                    dolore. A harum accusantium. Perferendis aut odio sunt
+                    asperiores repudiandae et. Eius et similique quidem voluptas
+                    nihil possimus natus commodi. Optio est quas. Tempora at
+                    ipsa ipsum deserunt dolorem explicabo. Ab omnis mollitia
+                    voluptas. Quae voluptas ullam at et optio id alias dolorem.
+                    Omnis totam sit adipisci vero. Deleniti praesentium commodi
+                    incidunt. Reprehenderit animi doloremque quia voluptatem
+                    ratione. Eaque ut et aut cupiditate et. Aut vero possimus
+                    animi porro. Fugit dolorum quis quidem debitis natus
+                    explicabo quibusdam aspernatur dolores. Vero consectetur
+                    maiores eaque incidunt consequatur tenetur neque dolore. A
+                    harum accusantium. Perferendis aut odio sunt asperiores
+                    repudiandae et. Eius et similique quidem voluptas nihil
+                    possimus natus commodi. Optio est quas. Tempora at ipsa
+                    ipsum deserunt dolorem explicabo.
+                  </p>
                 </div>
               </div>
             </div>
+
             <form className="bg-[#fefdf8] absolute z-50 inset-5 md:static p-3 hidden md:flex flex-col justify-between gap-3">
               <div className="flex flex-col">
                 <h1 className="mb-3 text-lg">Add a todo</h1>
